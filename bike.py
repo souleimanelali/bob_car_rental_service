d141453a9391ac912053d8b6fb7055d93557574a
souleimanelali <souleiman.elali@gmail.com>
2025-02-27 21:59:23 +0200

    Add Bike subclass inherits Vehicle class

---
+++ bike.py
@@ -0,0 +1,9 @@
+from Vehicle import Vehicle
+
+class Bike(Vehicle):
+    def __init__(self, brand, model, year, rental_price_per_day, engine_capacity):
+        super().__init__(brand, model, year, rental_price_per_day)
+        self.engine_capacity = engine_capacity
+
+    def display_info(self):
+        print(f"Bike: {self.brand} {self.model}, Year: {self.year}, Engine: {self.engine_capacity}cc, Rental Price: ${self.get_rental_price()}/day")
